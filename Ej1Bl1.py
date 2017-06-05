77099beca3cd1753a8948d5830609653f08471c4
juancarlos <juanki12>
2017-06-05 12:53:51 +0200

    06.06

--- Ej1Bl1.py
+++ Ej1Bl1.py
@@ -30,6 +30,7 @@ class ARPPoisoning(threading.Thread):
             send(arpPacket, verbose=False, loop=1, iface=str(self.IFc))
         except:
             print "***************************************"
+            print "***************************************"
             print "Unexpected error:", sys.exc_info()[0]
 
 
